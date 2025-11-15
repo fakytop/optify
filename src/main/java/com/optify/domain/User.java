fe7f187a7fde690adab674baa65b587c7522871f
Gonzalo Perez <prgonzalo94@gmail.com>
2025-11-15 20:25:20 -0300

    se agrega conexion

--- src/main/java/com/optify/domain/User.java
+++ src/main/java/com/optify/domain/User.java
@@ -1,8 +1,14 @@
 package com.optify.domain;
 
 import com.optify.exceptions.AuthenticationException;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
     private long ci;
     private String name;
     private String lastName;
